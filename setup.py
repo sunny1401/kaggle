cd2652a9b9695acd1c935488ed695406088e048e
Sankranti Joshi <sankrantijoshi14@gmail.com>
2023-03-11 16:38:20 +0000

    Adding vesion update to setup and pyproject

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 setup(
     name="kaggle_cli_wrapper",
     description='Kaggle CLI Wrapper',
-    version="1.1",
+    version="0.2",
     long_description=('Wrapper over Kaggle official API for ease of use '
     'while downloading competition datasets and general datasets.'
     ),
